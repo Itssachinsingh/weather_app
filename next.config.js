44c72367e56333c0a3cc881d59ff695be3c5151c
Sachin <sachingusain740@gmail.com>
2025-03-15 23:20:01 +0530

    second

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      unoptimized: true, // Disables Next.js Image Optimization
+    },
+    experimental: {
+      appDir: true, // Only if you're using the App Router
+    },
+  };
+  
+  module.exports = nextConfig;
+  
